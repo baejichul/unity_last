3f94969e8f4707988a4585de58ace17d070fe975
baejichul <jayseebae@naver.com>
2022-10-14 15:44:34 +0900

    1014

---
+++ Assets/MultiplayerRPG/Basic/NetMonster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityStandardAssets._2D;
+using UnityEngine.UI;
+
+public class NetMonster : NetworkBehaviour
+{
+    SpriteRenderer _sr;
+
+    void Start()
+    {
+        SetDefaultSortOrder();
+    }
+
+    void SetDefaultSortOrder()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+        if (_sr != null)
+            _sr.sortingOrder = 1;
+    }
+
+}
